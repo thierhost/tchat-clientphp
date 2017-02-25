9992b930dcf803d6128e70788df933a1da83f703
thierno thiam <thiernothiam@esp.sn>
2017-02-25 09:47:41 +0000

    add body content

--- discussions.php
+++ discussions.php
@@ -23,6 +23,26 @@
     </style>
 </head>
 <body>
+<div class="container-fluid">
+        <div class="row">
+                <div class="col-md-6 messageliste">
+
+                    </div>
+                <div class="col-md-6">
+                       <div class="col-md-offset-2 col-md-10">
+                                <blockquote>
+                                        <textarea name="sms" id="sms" cols="4"  class="form-control"></textarea>
+                                    </blockquote>
+                            </div>
+                       <div class="col-md-offset-9 col-md-3">
+                                <button type="button" class="btn btn-success" id="envoyer" >Envoyer</button>
+                            </div>
+
+                   </div>
+            </div>
+    +</div>
+
+
 
 
 
